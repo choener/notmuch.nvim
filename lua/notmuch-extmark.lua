6596c95a02caa87f79aa952389f48127a47334f1
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2023-08-20 17:49:03 +0200

    prepare to be able to open neomutt on message-id's

--- lua/notmuch-extmark.lua
+++ lua/notmuch-extmark.lua
@@ -1,5 +1,3 @@
---nm = require'notmuch/init.lua'
-
 -- TODO disable overlay when on the message line
 
 local M = {}
@@ -18,7 +16,7 @@ function M.queryById(msgid)
   local command = 'notmuch show --format=json --entire-thread=false id:'..msgid
   local handle = io.popen(command)
   local result = {}
-  if handle ~= nil
+  if handle
     then
       result = vim.json.decode(handle:read('*a'))
       handle:close()
@@ -26,32 +24,43 @@ function M.queryById(msgid)
   return result
 end
 
-function M.findMessageIds()
+function M.openNeomutt()
+  -- 1. Be over a Message-ID line
+  -- 2. Execute: notmuch-mutt search id:'the-id'
+  -- 3. Execute: neomutt -f ~/.cache/notmuch/mutt/results/
+  -- 4. Profit ;-)
+end
+
+function M.replaceMessageId()
   local lines = vim.api.nvim_buf_get_lines(0,0,-1, false)
   for row, line in pairs(lines)
     do
       -- Uses lua patterns, careful these are not regexes
       -- https://neovim.io/doc/user/luaref.html#lua-pattern
       -- https://www.lua.org/pil/20.2.html
-      local from, id1, id2, to = line:match('()%`%`Message%-ID%:%s?%<(%w+)%@(%w+)%>%s*%`%`()')
+      local from, how, idstr, to = line:match('()%`%`(%a*)%s*Message%-ID%:%s*%<([^%>]+)%>%s*%`%`()')
       if from
       then
-        local msg = M.queryById(id1..'@'..id2)
+        local msg = M.queryById('\''..idstr..'\'')
         local msgline = ''
-        local mail1 = msg[1][1][1]
+        local mail1 = nil
         local len = 0
-        if mail1 ~= nil
+        if (msg and msg[1] and msg[1][1] and msg[1][1][1])
           then
+            mail1 = msg[1][1][1]
             len = math.max(0, to-from+1)
-            msgline = string.format('%s   %s', mail1.headers.Date, mail1.headers.Subject)
+            -- Date: Tue, 18 Nov 2014 15:57:11 +0000
+            local date = vim.fn.strptime('%a, %d %b %Y %T %z', mail1.headers.Date)
+            local strdate = vim.fn.strftime('%F %T', date)
+            msgline = string.format('%s  %s', strdate, mail1.headers.Subject)
           end
-        -- TODO call to notmuch to get the actual message
-        --print(row,from,to,id1,'@',id2)
-        --print(vim.inspect(msg[1][1][1].headers))
-        -- TODO use own highlight group ...
         local opts = {
-          virt_text={{string.format('%-'..len..'.'..len..'s',msgline), 'EmailOneLine'}}, -- 'String' is the highlight group
-          virt_text_pos='overlay'
+          virt_text = {{string.format('%-'..len..'.'..len..'s',msgline), 'EmailOneLine'}},
+          virt_text_pos = 'overlay'
+        }
+        local opts = {
+          virt_text = { {string.format('%-'..len..'s', msgline), 'EmailOneLine'} },
+          virt_text_pos = 'overlay'
         }
         -- https://jdhao.github.io/2021/09/09/nvim_use_virtual_text/
         --
